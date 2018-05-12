cd4a9a31001aee1c55421506c59ed6599dcec42a
SMuyesser <smuyesser@gmail.com>
2018-05-12 14:10:31 -0700

    updated client origin in config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-	exports.CLIENT_ORIGIN = 'https://smuyesser.github.io/Hungry-Recipes';
+	exports.CLIENT_ORIGIN = 'https://smuyesser.github.io/hungry-recipes-v2';
 	exports.PORT = process.env.PORT || 8000;
 	exports.API_KEY = process.env.API_KEY;
